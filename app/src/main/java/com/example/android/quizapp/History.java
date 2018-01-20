31ec6aaba84db8a6b76e9d16023b9656e945cd3f
Vouts <thevouts@gmail.com>
2018-01-20 20:14:14 +0200

    V1

--- app/src/main/java/com/example/android/quizapp/History.java
+++ app/src/main/java/com/example/android/quizapp/History.java
@@ -2,12 +2,50 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class History extends MainActivity {
+public class History extends AppCompatActivity {
+    String category, username;
+
+    /**
+     * Saves Current state
+     */
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putString("typed_name", username);
+        savedInstanceState.putString("selected_category", category);
+
+        super.onSaveInstanceState(savedInstanceState);
+
+    }
+
+    /**
+     * Restore saved state
+     */
+
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+
+        if (saveInstanceState != null) {
+
+            category = saveInstanceState.getString("selected_category");
+            username = saveInstanceState.getString("typed_name");
+
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        category = getIntent().getStringExtra("EXTRA_SELECTED_CATEGORY");
+        username = getIntent().getStringExtra("EXTRA_TYPED_NAME");
+
     }
 }
--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
     //Definition of global variables
-    String category;
+    String category, username;
 
     /**
      * Saves Current state
@@ -20,8 +24,14 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
 
-        savedInstanceState.putString("selected_category", category);
+        Spinner spinner = findViewById(R.id.categories);
+        savedInstanceState.putInt("selected_category", spinner.getSelectedItemPosition());
 
+        EditText name =  findViewById(R.id.name);
+        username = name.getText().toString();
+        savedInstanceState.putString("typed_name", username);
+
+        super.onSaveInstanceState(savedInstanceState);
 
     }
 
@@ -32,42 +42,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle saveInstanceState) {
         super.onRestoreInstanceState(saveInstanceState);
 
-        category = saveInstanceState.getString("selected_category");
+        if (saveInstanceState != null) {
+
+            username = saveInstanceState.getString("typed_name");
+
+            EditText name = (EditText) findViewById(R.id.name);
+            name.setText(username);
+
+            Spinner spinner = findViewById(R.id.categories);
+            spinner.setSelection(saveInstanceState.getInt("selected_category", 0));
+        }
 
     }
+
+
 
     /**
      * When the user click "Get started" button
      *
-     * 1. Gets the selected category
+     * 1. Check if the players name is set
      *
-     * 2. If no category is selected, it will will pass the first category of
+     * 2. Gets the selected category
+     *
+     * 3. If no category is selected, it will will pass the first category of
      * the string array "categories"
      *
-     * 3. Starts the next activity and passes the needed global vars on it.
+     * 4. Starts the next activity and passes the needed global vars on it.
      *
      * */
     public void GetStarted(View view) {
         //1.
-        Spinner start = (Spinner) findViewById(R.id.categories);
-        category = start.getSelectedItem().toString();
-        //2.
-        if (category == null){
-            Resources res = getResources();
-            String[] categories = res.getStringArray(R.array.Categories);
-            category = categories[0];
-        }
+        EditText name =  findViewById(R.id.name);
+        username = name.getText().toString();
+        if(username.length()>0) {
+            //2.
+            Spinner start = (Spinner) findViewById(R.id.categories);
+            category = start.getSelectedItem().toString();
+            //3.
+            if (category == null) {
+                Resources res = getResources();
+                String[] categories = res.getStringArray(R.array.Categories);
+                category = categories[0];
+            }
 
-        //3.
-        String activityToStart = "com.example.android.quizapp." + category;
-        try {
-            Class<?> chosenCategory = Class.forName(activityToStart);
-            Intent intent = new Intent(this, chosenCategory);
-            intent.putExtra("EXTRA_SELECTED_CATEGORY", category);
-            startActivity(intent);
-        } catch (ClassNotFoundException ignored) {
-            Toast toast = Toast.makeText(this, "class not found", Toast.LENGTH_SHORT);
-            toast.show();
+            //4.
+            String activityToStart = "com.example.android.quizapp." + category;
+            try {
+                Class<?> chosenCategory = Class.forName(activityToStart);
+                Intent intent = new Intent(this, chosenCategory);
+                intent.putExtra("EXTRA_SELECTED_CATEGORY", category);
+                intent.putExtra("EXTRA_TYPED_NAME", username);
+                startActivity(intent);
+            } catch (ClassNotFoundException ignored) {
+                Toast toast = Toast.makeText(this, "class not found", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } else {
+            Toast getName = Toast.makeText(this,R.string.namecheck,Toast.LENGTH_LONG);
+            getName.show();
         }
 
     }
